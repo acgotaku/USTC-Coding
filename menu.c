4e838c735fa4e5a9214d1bc80329af6c703b3fb8
acgotaku <acgotaku311@gmail.com>
2014-09-08 21:09:52 +0800

    module develop

--- menu.c
+++ menu.c
@@ -26,21 +26,37 @@
 #define DESC_LEN    1024
 #define CMD_LEN    10
 #define CMD_NUM    10
-
+void Init();
+void SelectCmd(char * cmd);
 typedef struct DataNode
 {
     char     cmd[CMD_LEN];
     char    desc[DESC_LEN];
     struct  DataNode *next;
 } tDataNode;
-
-main()
+char * list[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+tDataNode *head = NULL; 
+tDataNode * p = NULL;
+int main()
+{   
+    Init();
+    while(1)
+    {
+        char cmd[CMD_LEN];
+        printf("Input a cmd string > ");
+        scanf("%s", &cmd);
+        if(strcmp("exit",cmd)==0)
+        {
+            return 0;
+        }
+        SelectCmd(cmd);
+    }
+}
+void Init()
 {
-    char * list[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
-    tDataNode *head = NULL;
+    
     /* Init cmd list */
     int i;
-    tDataNode * p = NULL;
     for (i=0; i<CMD_NUM; i++)
     {
         printf("%s",list[i]);
@@ -57,30 +73,23 @@ main()
         printf("%s - %s\n", p->cmd, p->desc);
         p = p->next;
     }
-    printf("exit - exit program\n");
+    printf("exit - exit program!\n");
+}
+void SelectCmd(char * cmd)
+{
     /* cmd line begins */
-    while(1)
+    p = head;
+    while(p != NULL)
     {
-        char cmd[CMD_LEN];
-        printf("Input a cmd string > ");
-        scanf("%s", &cmd);
-        if(strcmp("exit",cmd)==0){
-            return 0;
-        }
-        p = head;
-        while(p != NULL)
+        if(strcmp(p->cmd,cmd)==0)
         {
-            if(strcmp(p->cmd,cmd)==0)
-            {
-                printf("%s - %s\n", p->cmd, p->desc);
-                break;
-            }
-            p = p->next;
+            printf("%s - %s\n", p->cmd, p->desc);
+            break;
         }
-        if(!p)
-        {
-            printf("This is a wrong cmd string!\n ");
-            continue;
-        }
+        p = p->next;
+    }
+    if(!p)
+    {
+        printf("This is a wrong cmd string!\n ");
     }
 }
